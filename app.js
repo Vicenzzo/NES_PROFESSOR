0baa4a846121f3ed4d0ba6aa237f162c2acfa564
Vicenzzo Toth Amorim <vicenzzo.toth@gmail.com>
2022-09-21 04:56:51 -0300

    Commit três telas

---
+++ app.js
@@ -0,0 +1,9 @@
+var $input = document.getElementById('input-file'),
+$fileName = document.getElementById('file-name'),
+$input = document.getElementById('input-file'),
+$fileName = document.getElementById('file-name') 
+;
+
+$input.addEventListener('change', function(){
+    $fileName.textContent = this.value;
+})
